6007def18b0f3cc45e8042e892f6f127d3e7b85f
Mengbill <36590590+Mengbill@users.noreply.github.com>
2025-07-16 16:17:38 +0800

    新增: 升级版本至1.21.7

--- src/main/java/org/azurith/anotherbedwars/AnotherBedWars.java
+++ src/main/java/org/azurith/anotherbedwars/AnotherBedWars.java
@@ -2,6 +2,7 @@ package org.azurith.anotherbedwars;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.apache.commons.io.FileUtils;
 import org.azurith.anotherbedwars.game.GameListener;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Biome;
@@ -14,6 +15,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Collections;
@@ -26,77 +28,93 @@ public final class AnotherBedWars extends JavaPlugin {
 
     @Override
     public void onLoad() {
-        getLogger().log(Level.WARNING,"正在为插件运行配置所需环境");
-            try {
-                File file = new File(Bukkit.getServer().getWorldContainer(), "server.properties");
-                List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
-                for (int i = 0; i < lines.size(); i++) {
-                    if (lines.get(i).equalsIgnoreCase("allow-nether=true")) {
-                        getLogger().log(Level.WARNING,"正在禁用世界 Nether");
-                        lines.set(i, "allow-nether=false");
-                        Files.write(file.toPath(), lines, StandardCharsets.UTF_8);
-                        getLogger().log(Level.WARNING,"已修改 server.properties -> allow-nether=false");
-                        restartNeeded = true;
-                    }
-                    if (lines.get(i).startsWith("level-name") && !lines.get(i).equalsIgnoreCase("level-name=AnotherBedWars_Arena")) {
-                        getLogger().log(Level.WARNING,"正在配置世界 AnotherBedWars_Arena 为主世界");
-                        lines.set(i, "level-name=AnotherBedWars_Arena");
-                        Files.write(file.toPath(), lines, StandardCharsets.UTF_8);
-                        getLogger().log(Level.WARNING,"已修改 server.properties -> level-name=AnotherBedWars_Arena");
-                        restartNeeded = true;
-                    }
+        getLogger().log(Level.WARNING, "正在为插件运行配置所需环境");
+        try {
+            File file = new File(Bukkit.getServer().getWorldContainer(), "server.properties");
+            List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+            boolean modified = false;
+            for (int i = 0; i < lines.size(); i++) {
+                if (lines.get(i).equalsIgnoreCase("allow-nether=true")) {
+                    getLogger().log(Level.WARNING, "正在禁用世界 Nether");
+                    lines.set(i, "allow-nether=false");
+                    modified = true;
                 }
-            } catch (Exception e) {
-                getLogger().log(Level.SEVERE,"尝试操作 server.properties 时发生错误",e);
-                Bukkit.getPluginManager().disablePlugin(this);
+                if (lines.get(i).startsWith("level-name") && !lines.get(i).equalsIgnoreCase("level-name=world_the_void")) {
+                    getLogger().log(Level.WARNING, "正在配置世界 world_the_void 为主世界");
+                    lines.set(i, "level-name=world_the_void");
+                    modified = true;
+                }
             }
-            try {
-                File bukkitFile = new File(Bukkit.getServer().getWorldContainer(), "bukkit.yml");
-                YamlConfiguration bukkitYaml = YamlConfiguration.loadConfiguration(bukkitFile);
-                if (bukkitYaml.getBoolean("settings.allow-end")) {
-                    getLogger().log(Level.WARNING,"正在禁用世界 End");
-                    bukkitYaml.set("settings.allow-end", false);
-                    bukkitYaml.save(bukkitFile);
-                    getLogger().log(Level.WARNING,"已修改 bukkit.yml -> allow-end=false");
-                    restartNeeded = true;
-                }
-                String generator = bukkitYaml.getString("worlds.AnotherBedWars_Arena.generator");
-                if (generator == null || !generator.equals("AnotherBedWars")) {
-                    getLogger().log(Level.WARNING, "正在为世界 AnotherBedWars_Arena 配置 Generator");
-                    bukkitYaml.set("worlds.AnotherBedWars_Arena.generator", "AnotherBedWars");
-                    bukkitYaml.save(bukkitFile);
-                    getLogger().log(Level.WARNING,"已修改 bukkit.yml -> worlds.AnotherBedWars_Arena.generator=AnotherBedWars");
-                    restartNeeded = true;
-                }
-            } catch (Exception e) {
-                getLogger().log(Level.SEVERE,"尝试操作 bukkit.yml 时发生错误",e);
-                Bukkit.getPluginManager().disablePlugin(this);
+            if (modified) {
+                Files.write(file.toPath(), lines, StandardCharsets.UTF_8);
+                getLogger().log(Level.WARNING, "已修改 server.properties 配置");
+                restartNeeded = true;
             }
+        } catch (Exception e) {
+            getLogger().log(Level.SEVERE, "尝试操作 server.properties 时发生错误", e);
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+        try {
+            File bukkitFile = new File(Bukkit.getServer().getWorldContainer(), "bukkit.yml");
+            YamlConfiguration bukkitYaml = YamlConfiguration.loadConfiguration(bukkitFile);
+            boolean modified = false;
+            if (bukkitYaml.getBoolean("settings.allow-end")) {
+                getLogger().log(Level.WARNING, "正在禁用世界 End");
+                bukkitYaml.set("settings.allow-end", false);
+                modified = true;
+            }
+            String generator = bukkitYaml.getString("worlds.world_the_void.generator");
+            if (generator == null || !generator.equals("AnotherBedWars")) {
+                getLogger().log(Level.WARNING, "正在为世界 world_the_void 配置 VoidGenerator");
+                bukkitYaml.set("worlds.world_the_void.generator", "AnotherBedWars");
+                modified = true;
+            }
+            if (modified) {
+                bukkitYaml.save(bukkitFile);
+                getLogger().log(Level.WARNING, "已修改 bukkit.yml 配置");
+                restartNeeded = true;
+            }
+        } catch (Exception e) {
+            getLogger().log(Level.SEVERE, "尝试操作 bukkit.yml 时发生错误", e);
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
     }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         if (restartNeeded) {
-            getLogger().log(Level.WARNING,"插件所需运行环境已配置完成 即将重启服务器以应用配置更改");
+            getLogger().log(Level.WARNING, "即将重启服务器以应用配置更改");
             Bukkit.getScheduler().runTaskLater(this, Bukkit::restart, 20);
             return;
         }
-        Bukkit.getPluginManager().registerEvents(new GameListener(),this);
-        getLogger().log(Level.WARNING,"插件所需运行环境已配置完成 正在启动插件");
+        String[] useless_worlds = {"world", "world_nether", "world_the_end"};
+        for (String world : useless_worlds) {
+            File worldFolder = new File(Bukkit.getServer().getWorldContainer(), world);
+            if (worldFolder.exists() && worldFolder.isDirectory()) {
+                try {
+                    FileUtils.deleteDirectory(worldFolder);
+                    getLogger().log(Level.WARNING, "正在清理世界 " + world);
+                } catch (IOException e) {
+                    getLogger().log(Level.SEVERE, "尝试清理世界 " + world + " 时发生错误", e);
+                }
+            }
+        }
+        Bukkit.getPluginManager().registerEvents(new GameListener(), this);
+        getLogger().log(Level.WARNING, "插件所需运行环境已配置完成 正在启动插件");
         Bukkit.getConsoleSender().sendMessage(Component.newline()
                 .appendNewline()
                 .append(Component.text(
-                        """
-                                 █████╗ ███╗   ██╗ ██████╗ ████████╗██╗  ██╗███████╗██████╗   ██████╗ ███████╗██████╗ ██╗    ██╗ █████╗ ██████╗ ███████╗
-                                ██╔══██╗████╗  ██║██╔═══██╗╚══██╔══╝██║  ██║██╔════╝██╔══██╗  ██╔══██╗██╔════╝██╔══██╗██║    ██║██╔══██╗██╔══██╗██╔════╝
-                                ███████║██╔██╗ ██║██║   ██║   ██║   ███████║█████╗  ██████╔╝  ██████╔╝█████╗  ██║  ██║██║ █╗ ██║███████║██████╔╝███████╗
-                                ██╔══██║██║╚██╗██║██║   ██║   ██║   ██╔══██║██╔══╝  ██╔══██╗  ██╔══██╗██╔══╝  ██║  ██║██║███╗██║██╔══██║██╔══██╗╚════██║
-                                ██║  ██║██║ ╚████║╚██████╔╝   ██║   ██║  ██║███████╗██║  ██║  ██████╔╝███████╗██████╔╝╚███╔███╔╝██║  ██║██║  ██║███████║
-                                ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝  ╚═════╝ ╚══════╝╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
-                                """,NamedTextColor.GREEN)
-                .appendNewline()
-                .append(Component.text("                             Enabled AnotherBedWars v1.0 by Mengbill",NamedTextColor.BLUE)))
+                                """
+                                         █████╗ ███╗   ██╗ ██████╗ ████████╗██╗  ██╗███████╗██████╗   ██████╗ ███████╗██████╗ ██╗    ██╗ █████╗ ██████╗ ███████╗
+                                        ██╔══██╗████╗  ██║██╔═══██╗╚══██╔══╝██║  ██║██╔════╝██╔══██╗  ██╔══██╗██╔════╝██╔══██╗██║    ██║██╔══██╗██╔══██╗██╔════╝
+                                        ███████║██╔██╗ ██║██║   ██║   ██║   ███████║█████╗  ██████╔╝  ██████╔╝█████╗  ██║  ██║██║ █╗ ██║███████║██████╔╝███████╗
+                                        ██╔══██║██║╚██╗██║██║   ██║   ██║   ██╔══██║██╔══╝  ██╔══██╗  ██╔══██╗██╔══╝  ██║  ██║██║███╗██║██╔══██║██╔══██╗╚════██║
+                                        ██║  ██║██║ ╚████║╚██████╔╝   ██║   ██║  ██║███████╗██║  ██║  ██████╔╝███████╗██████╔╝╚███╔███╔╝██║  ██║██║  ██║███████║
+                                        ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝  ╚═════╝ ╚══════╝╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+                                        """, NamedTextColor.GREEN)
+                        .appendNewline()
+                        .append(Component.text("                             Enabled AnotherBedWars v1.0 by Mengbill", NamedTextColor.BLUE)))
                 .appendNewline()
         );
     }
@@ -108,9 +126,11 @@ public final class AnotherBedWars extends JavaPlugin {
     }
 
     @Override
-    public ChunkGenerator getDefaultWorldGenerator(@NotNull String worldName, String id) {return new VoidGenerator();}
+    public ChunkGenerator getDefaultWorldGenerator(@NotNull String worldName, String id) {
+        return new VoidGenerator();
+    }
 
-    public static class VoidGenerator extends ChunkGenerator{
+    public static class VoidGenerator extends ChunkGenerator {
         @Override
         public @Nullable BiomeProvider getDefaultBiomeProvider(@NotNull WorldInfo worldInfo) {
             return new BiomeProvider() {
